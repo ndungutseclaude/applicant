f95b9952d77c1ab3f0f96c7273b332013af99c8e
claudendungutse <ndclaude5@gmail.com>
2019-10-31 16:43:25 +0100

    addition of client-instal

--- src/action/questionAction.js
+++ src/action/questionAction.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const getQuestions = () => async dispatch =>{
     //console.log('ACTION IS WORKING')
     const response = await axios.get('/api/')
-    //console.log(response)
+    console.log(response)
     dispatch({
         type: FETCH_QUESTIONS,
         payload: response.data
